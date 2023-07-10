a97dccbf511497eda3ed3cfc250b61f58fd6cf6d
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-10 12:09:08 -0400

    allow localhost

--- nifty/settings.py
+++ nifty/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-=@nh6mk75o6cfld6nu9805mfyzv*4(m-qy!s9-+uslu8%a+pd$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nft-mint-api-824f9dc02cba.herokuapp.com']
+ALLOWED_HOSTS = ['nft-mint-api-824f9dc02cba.herokuapp.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
