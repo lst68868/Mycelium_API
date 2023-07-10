c1bcae68d97c86e2860b3df014891186ad0dfadf
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-10 16:00:04 -0400

    token

--- nifty/settings.py
+++ nifty/settings.py
@@ -23,7 +23,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -47,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'main_app'
 ]
 
--- nifty/urls.py
+++ nifty/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from main_app.views import NFTViewSet, UserViewSet
 from rest_framework import routers
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'NFT', NFTViewSet)
@@ -11,4 +12,9 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
+]
+
+
+urlpatterns += [
+    path('api-token-auth/', views.obtain_auth_token)
 ]
