dfb39e25b921966676b4728ab52e4d85d2da3bb6
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-14 09:29:37 -0400

    f

--- nifty/settings.py
+++ nifty/settings.py
@@ -208,7 +208,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',  # Add your React frontend URL here
+    'http://localhost:3000', 'netlify.app' # Add your React frontend URL here
 ]
 CORS_ALLOW_HEADERS = [
     'accept',
